8028cc24a37bf7877447b6ed185e8ee406b17d91
Ken Layug <ken_layug@yahoo.com>
2016-10-07 07:52:06 +0800

    fixed order transaction ui.

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,7 @@
     'use strict';
     
     var app = angular
-        .module("app", ['ui.router', 'datatables'])
+        .module("app", ['ui.router', 'datatables', 'ui.materialize'])
         .config(function($stateProvider, $urlRouterProvider){
        
         $stateProvider
--- js/app/controllers/transaction/productOrderCtrl.ctr.js
+++ js/app/controllers/transaction/productOrderCtrl.ctr.js
@@ -5,12 +5,15 @@
         .module('app')
         .controller('productOrderCrtl', productOrderCrtl);
     
-    function productOrderCrtl(){
+    function productOrderCrtl($timeout, $filter){
         var vm = this;
-        vm.orderList = [{}];
+        var productList 		=	[];
+        var brandList 			=	[];
+        var categoryList 		=	[];
+        vm.orderList = [];
         vm.quantity = 0;
         vm.totalPrice = 0;
-        vm.persons = [{
+        vm.productList = [{
 				    "id": 860,
 				    "image": "images/pormon.jpg",
 				    "firstName": "Superman",
@@ -40,17 +43,82 @@
 				    "category": "Awesome",
 				    "price": 100
 				}];
+
+		var productRef = firebase.database().ref('products');
+		var categoryRef = firebase.database().ref('categories');
+		var brandRef = firebase.database().ref('brands');
+
+		brandRef.on('value', function(data) {
+        	brandList = [];
+
+        	$timeout(function() {
+        		data.forEach(function(childData) {
+        			brandList.push({
+        				id: childData.key,
+        				name: childData.val().name
+        			});
+                    brandList       =   $filter('orderBy')(brandList, 'name', false);
+        		});
+
+        		vm.brands = brandList;
+        	});
+        });
+
+        categoryRef.on('value', function(data) {
+        	categoryList = [];
+
+        	$timeout(function() {
+        		data.forEach(function(childData) {
+        			categoryList.push({
+        				id: childData.key,
+        				name: childData.val().name
+        			});
+                    categoryList       =   $filter('orderBy')(categoryList, 'name', false);
+        		});
+
+        		vm.categories = categoryList;
+        	});
+        });
+
+		productRef.on('value', function(data) {
+        	productList = [];
+
+        	$timeout(function() {
+        		data.forEach(function(childData) {
+        			productList.push({
+        				id: childData.key,
+        				name: childData.val().name,
+        				price: childData.val().price
+        			});
+                    productList       =   $filter('orderBy')(productList, 'name', false);
+        		});
+
+        		vm.products = productList;
+        		console.log(vm.products);
+        	});
+        });
         
-        vm.addToCart = function(index){
-          vm.orderList.push({
-             orderName: vm.persons[index].firstName,
-             quantity: vm.quantity,
-             price: vm.quantity*vm.persons[index].price
-          }); 
-            
-         vm.orderList.forEach(function(){
-             vm.totalPrice += vm.orderList.price;
-         });
+        vm.addToCart = function(product, detail){
+
+        	var orderExist 		=	false;
+        	vm.orderList.forEach(function(order){
+        		if (order.product.id == product.id){
+        			order.quantity += detail.quantity;
+        			orderExist 		=	true;
+        		}
+        	});
+
+        	if (!orderExist){
+	        	vm.orderList.push({
+		             product: product,
+		             quantity: detail.quantity
+		        });
+		    }
+	         
+	        vm.totalPrice += (product.price * detail.quantity);
+	        detail.quantity 		=	'';
         };
+
+
     }
 })(); 
