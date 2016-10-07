f2d6302433e14aad8ca7e14e9b5b3392fba5b0b2
Ken Layug <ken_layug@yahoo.com>
2016-10-07 11:56:53 +0800

    fixed customer's order.

--- js/app/controllers/transaction/customerOrderCtrl.ctr.js
+++ js/app/controllers/transaction/customerOrderCtrl.ctr.js
@@ -22,7 +22,9 @@
                     productList.push({
                         id: childData.key,
                         name: childData.val().name,
-                        price: childData.val().price
+                        price: childData.val().price,
+                        imageUrl: childData.val().imageUrl != null ? childData.val().imageUrl
+                            : 'http://alphagled.com/wp-content/themes/456ecology/assets//img/no-product-image.png'
                     });
 
                 });
@@ -31,40 +33,52 @@
 
         ordersRef.once('value')
         	.then(function(data) {
-        		data.forEach(function(childData) {
-        			if(childData.val().userId == customerId) {
-        				userOrderList.push(childData.val());
-        			}
-        		});
+                $timeout(function(){
+            		data.forEach(function(childData) {
+            			if(childData.val().userId == customerId) {
+            				userOrderList.push(childData.val());
+            			}
+            		});
 
-        		vm.userOrders         =   userOrderList;
-                vm.userOrders.forEach(function(order){
+            		vm.userOrders         =   userOrderList;
+                    vm.userOrders.forEach(function(order){
 
-                    order.orderDate        =   moment(order.order_date).format('MMM D, YYYY');
-                    order.totalPrice        =   0;
-                    order.orders = Object.keys(order.orders).map(function(k) { return order.orders[k] });
-                    order.orders.forEach(function(orderDetail){
+                        order.orderDate        =   moment(order.order_date).format('MMM D, YYYY');
+                        order.totalPrice        =   0;
+                        order.orders = Object.keys(order.orders).map(function(k) { return order.orders[k] });
+                        order.orders.forEach(function(orderDetail){
 
-                        productList.forEach(function(product){
+                            productList.forEach(function(product){
 
-                            if (orderDetail.productId == product.id){
+                                if (orderDetail.productId == product.id){
 
-                                orderDetail.productName = product.name;
-                                orderDetail.productPrice = product.price;
-                                order.totalPrice += (orderDetail.quantity * product.price);
+                                    orderDetail.productName = product.name;
+                                    orderDetail.productPrice = product.price;
+                                    orderDetail.productImage = product.imageUrl;
+                                    order.totalPrice += (orderDetail.quantity * product.price);
 
-                            }//end if
+                                }//end if
+
+                            });
+                            order.vat = order.totalPrice * .12;
 
                         });
 
                     });
-
-                });
-                vm.userOrders           =   $filter('orderBy')(vm.userOrders, 'orderDate', false);
-                console.log(vm.userOrders);               
+                    vm.userOrders           =   $filter('orderBy')(vm.userOrders, 'orderDate', false);
+                    console.log(vm.userOrders);
+                });             
 
         	}).catch(function(error) {
         		swal('Error', error.message, 'error');
         	})
+
+        vm.viewOrders           =   function(order){
+
+            $('#viewOrders').openModal();
+            vm.orders       =   $filter('orderBy')(order.orders, 'productName', false);
+            vm.totalPrice   =   order.totalPrice;
+
+        }
     }
 })(); 
