86e7533d9380d632e01dba13d306f5ed1b49fdd3
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-10 05:04:55 +0800

    Dashboard accessibility

--- public/js/app/controllers/dashboardCtrl.ctr.js
+++ public/js/app/controllers/dashboardCtrl.ctr.js
@@ -5,8 +5,28 @@
         .module('app')
         .controller('dashboardCtrl', dashboardCtrl);
     
-    function dashboardCtrl($rootScope, $location){
+    function dashboardCtrl($rootScope, $location, $state){
         var vm = this;
+        var auth = firebase.auth();
+        var userRef = firebase.database().ref('users');
         $rootScope.showSection = $location.path() == "/login";
+
+        auth.onAuthStateChanged(function(user) {
+            if(user) {
+                $('#log-out').show();
+                
+                userRef.child(user.uid).once('value', function(data) {
+                    $('#register').closeModal();
+
+                    if(!data.val().isAdmin) {
+                        $state.go('productOrder');
+                    }
+                });
+            } else {
+                $('#log-out').hide();
+
+                $state.go('login');
+            }
+        });
     }
 })(); 
--- public/js/app/controllers/loginCtrl.ctr.js
+++ public/js/app/controllers/loginCtrl.ctr.js
@@ -13,6 +13,7 @@
         auth.onAuthStateChanged(function(user) {
             if(user) {
                 $('#log-out').show();
+                $('#side-nav-btn').show();
                 
                 userRef.child(user.uid).once('value', function(data) {
                     $('#register').closeModal();
@@ -25,15 +26,20 @@
                 });
             } else {
                 $('#log-out').hide();
+                $('#side-nav-btn').hide();
 
                 $state.go('login');
-            }
+            } 
         });
 
         vm.login = function(user){
+            $('.btn').prop('disabled', true);
+
             firebase.auth().signInWithEmailAndPassword(vm.user.email, vm.user.pass)
                 .catch(function(error) {
                     swal('Error', error.message, 'error');
+
+                    $('.btn').prop('disabled', false);
                 });
         };
         
